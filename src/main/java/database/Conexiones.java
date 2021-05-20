059cd38e79ccba2ebe28d5ba970095f4854db02a
alejandro <alejandro@MrRobot>
2021-05-20 17:57:05 -0500

    diseño pag bienbenida, inicio, agendar

--- src/main/java/database/Conexiones.java
+++ src/main/java/database/Conexiones.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @author Vinni
  */
 public class Conexiones {
-    private final String URL = "C:/APL/basedatos.db";
+    private final String URL = "db/basedatos.db";
     
     public Connection conectarse(){
         try {
